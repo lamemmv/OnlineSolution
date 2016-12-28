931803921c0c7226a8606901f5f7949101109ac7
lamemmv <lamemmv@gmail.com>
2016-12-28 12:32:30 +0700

    update album wedding

--- OS.Web/OS.Web/app/shared/utils/config.service.ts
+++ OS.Web/OS.Web/app/shared/utils/config.service.ts
@@ -6,8 +6,8 @@ export class ConfigService {
     _apiURI : string;
 
     constructor() {
-        //this._apiURI = 'http://os.web.api/api/';
-        this._apiURI = 'http://localhost:5000/api/';
+        this._apiURI = 'http://os.web.api/api/';
+        //this._apiURI = 'http://localhost:5000/api/';
      }
 
      getApiURI() {
